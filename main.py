43a79295739dc393f93e48ae4bf5b36a7482f579
Petr Tsopa <petrtsopa03@gmail.com>
2025-03-18 06:23:46 +0300

    small improvements

--- main.py
+++ main.py
@@ -12,19 +12,16 @@ from map_data import MapData, Station
 
 
 def format_filename(filename):
-    return translit(filename.replace(' ', '_').replace('\n', '_'), "ru", reversed=True)
+    return translit(filename.replace(" ", "_").replace("\n", "_"), "ru", reversed=True)
 
 
 def draw_full_map(args):
     map_data = MapData(json.loads(args.map_data.read()), args.assets)
 
-    if not os.path.exists('output'):
-        os.mkdir('output')
-    for file in os.listdir('output'):
-        os.remove(os.path.join('output', file))
+    os.makedirs(os.path.dirname(os.path.abspath(args.output)), exist_ok=True)
 
     metro_map = map_data.draw()
-    metro_map.save(filename=args.output.name)
+    metro_map.save(filename=args.output)
 
 
 def draw_linear_map(args):
@@ -33,22 +30,37 @@ def draw_linear_map(args):
     if args.all:
         lines = map_data.lines
     else:
-        lines = [l for l in map_data.lines if l.name in args.lines]
+        lines = [line for line in map_data.lines if line.name in args.lines]
 
     if not lines:
         print("No lines to render")
         return
+
+    os.makedirs(args.output, exist_ok=True)
 
     for i, line in enumerate(lines):
         logging.info(f"[{i + 1} / {len(lines)}] Rendering {line.name}")
         for element in line.elements:
             if isinstance(element, Station):
                 for reverse_direction in [0, 1]:
-                    linear_metro_map = line.get_linear_metro_map(reverse_direction, element.name)
+                    linear_metro_map = line.get_linear_metro_map(
+                        reverse_direction, element.name
+                    )
 
-                    linear_metro_map.save(filename=os.path.join('output', format_filename('linear_' + line.name + '_' +
-                                                                                          element.name + '_' +
-                                                                                          str(reverse_direction) + '.png')))
+                    linear_metro_map.save(
+                        filename=os.path.join(
+                            args.output,
+                            format_filename(
+                                "linear_"
+                                + line.name
+                                + "_"
+                                + element.name
+                                + "_"
+                                + str(reverse_direction)
+                                + ".png"
+                            ),
+                        )
+                    )
 
     print(f"Rendered {len(lines)} lines")
 
@@ -56,27 +68,53 @@ def draw_linear_map(args):
 def main():
     parser = argparse.ArgumentParser(
         prog="metro-map-creator",
-        description="a tool for creating metro maps using json data"
+        description="a tool for creating metro maps using json data",
     )
 
-    subparsers = parser.add_subparsers(dest='command', help="Available commands", required=True)
+    subparsers = parser.add_subparsers(
+        dest="command", help="Available commands", required=True
+    )
 
     parent_parser = argparse.ArgumentParser(add_help=False)
-    parent_parser.add_argument('map_data', type=argparse.FileType('r'))
-    parent_parser.add_argument('assets', type=pathlib.Path, help="path to the asset folder")
-    parent_parser.add_argument('-v', "--verbose", action='count', default=0, help="verbosity")
+    parent_parser.add_argument("map_data", type=argparse.FileType("r"))
+    parent_parser.add_argument(
+        "assets", type=pathlib.Path, help="path to the asset folder"
+    )
+    parent_parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="verbosity"
+    )
 
-    full_parser = subparsers.add_parser('full', parents=[parent_parser], help="Draw the complete metro map")
-    full_parser.add_argument('-o', '--output', default='./metro_map.png', type=argparse.FileType('w'),
-                             help="name of the output file")
+    full_parser = subparsers.add_parser(
+        "full", parents=[parent_parser], help="Draw the complete metro map"
+    )
+    full_parser.add_argument(
+        "-o",
+        "--output",
+        default="./output/metro_map.png",
+        type=str,
+        help="name of the output file",
+    )
     full_parser.set_defaults(func=draw_full_map)
 
-    linear_parser = subparsers.add_parser('linear', parents=[parent_parser], help="Draw the linear map for a station")
-    linear_parser.add_argument('-l', '--lines', action='extend', nargs='+', default=[],
-                               help="name of the lines to be drawn")
-    linear_parser.add_argument('--all', action='store_true', help="render all lines")
-    linear_parser.add_argument('-o', '--output', default="./output", type=pathlib.Path,
-                               help="name of the output folder")
+    linear_parser = subparsers.add_parser(
+        "linear", parents=[parent_parser], help="Draw the linear map for a station"
+    )
+    linear_parser.add_argument(
+        "-l",
+        "--lines",
+        action="extend",
+        nargs="+",
+        default=[],
+        help="name of the lines to be drawn",
+    )
+    linear_parser.add_argument("--all", action="store_true", help="render all lines")
+    linear_parser.add_argument(
+        "-o",
+        "--output",
+        default="./output",
+        type=pathlib.Path,
+        help="name of the output folder",
+    )
     linear_parser.set_defaults(func=draw_linear_map)
 
     args = parser.parse_args()
@@ -90,7 +128,9 @@ def main():
 
     args.func(args)
 
-    print(f'Generating completed in {int((datetime.datetime.now() - start_time).total_seconds() * 1000)} ms')
+    print(
+        f"Generating completed in {int((datetime.datetime.now() - start_time).total_seconds() * 1000)} ms"
+    )
 
 
 if __name__ == "__main__":
--- utilities.py
+++ utilities.py
@@ -45,12 +45,15 @@ def place(image, image_to_place, coords, relative_to):
 
 
 def turn(direction, clockwise):
-    directions = ['LEFT', 'UP', 'RIGHT', 'DOWN']
-    for (num, cur_direction) in enumerate(directions):
+    directions = ["LEFT", "UP", "RIGHT", "DOWN"]
+    for num, cur_direction in enumerate(directions):
         if cur_direction == direction:
-            return directions[(num + 1) % len(directions)] if clockwise else \
-                directions[(num + len(directions) - 1) % len(directions)]
-    raise Exception('No such direction found')
+            return (
+                directions[(num + 1) % len(directions)]
+                if clockwise
+                else directions[(num + len(directions) - 1) % len(directions)]
+            )
+    raise Exception("No such direction found")
 
 
 def opposite(direction):
@@ -71,32 +74,46 @@ def move(coords, delta, direction):
 
 
 def move_by_image(coords, image, direction):
-    if direction in ['left', 'right']:
+    if direction in ["left", "right"]:
         return move(coords, image.width, Direction[direction.upper()])
     else:
         return move(coords, image.height, Direction[direction.upper()])
 
 
-def get_text_image(text, image, font_path, font_color=Color('black'), background_color=Color('#FFFFFF80')):
+def get_text_image(
+    text,
+    image,
+    font_path,
+    font_color=Color("black"),
+    background_color=Color("#FFFFFF80"),
+):
     padding_size = (5, 3)
 
-    if font_color == Color('black'):
+    if font_color == Color("black"):
         padding_size = (0, 0)
 
     draw = Drawing()
     draw.fill_color = font_color
     draw.font = font_path
     draw.font_size = 18
-    res_image = Image(width=int(draw.get_font_metrics(image, text, multiline=True).text_width + 2 * padding_size[0]),
-                      height=int(draw.get_font_metrics(image, text, multiline=True).text_height + 2 * padding_size[1]),
-                      background=background_color)
-    res_image.virtual_pixel = 'transparent'
+    res_image = Image(
+        width=int(
+            draw.get_font_metrics(image, text, multiline=True).text_width
+            + 2 * padding_size[0]
+        ),
+        height=int(
+            draw.get_font_metrics(image, text, multiline=True).text_height
+            + 2 * padding_size[1]
+        ),
+        background=background_color,
+    )
+    res_image.virtual_pixel = "transparent"
     draw.text(padding_size[0], 14 + padding_size[1], text)
     draw(res_image)
     return res_image
 
 
-def get_arrow_image(size, color=Color('black'), background=Color('white')):
+def get_arrow_image(size, color=Color("black"), background=Color("white")):
     image = Image(width=size, height=size, background=background)
 
     with Drawing() as draw:
@@ -112,11 +129,15 @@ def get_arrow_image(size, color=Color('black'), background=Color('white')):
     return image
 
 
-def get_direction_image(logo_image, last_station_name, font_path, reverse_direction=False):
+def get_direction_image(
+    logo_image, last_station_name, font_path, reverse_direction=False
+):
     last_station_name_image = get_text_image(last_station_name, logo_image, font_path)
-    image_width = 20 + 27 + 10 + logo_image.width + 10 + last_station_name_image.width + 20
+    image_width = (
+        20 + 27 + 10 + logo_image.width + 10 + last_station_name_image.width + 20
+    )
 
-    direction_image = Image(width=image_width, height=128, background=Color('white'))
+    direction_image = Image(width=image_width, height=128, background=Color("white"))
 
     cur_pos = 20
     if not reverse_direction:
@@ -127,12 +148,12 @@ def get_direction_image(logo_image, last_station_name, font_path, reverse_direct
         place(direction_image, last_station_name_image, (cur_pos, 64), RelativeTo.LEFT)
 
         with Drawing() as draw:
-            draw.stroke_color = Color('gray')
+            draw.stroke_color = Color("gray")
             draw.line((image_width - 1, 10), (image_width - 1, 118))
             draw.draw(direction_image)
     else:
         with Drawing() as draw:
-            draw.stroke_color = Color('gray')
+            draw.stroke_color = Color("gray")
             draw.line((0, 10), (0, 118))
             draw.draw(direction_image)
 
@@ -148,18 +169,27 @@ def get_direction_image(logo_image, last_station_name, font_path, reverse_direct
 
 
 def round_corner(image, radius, coords, positive_dx, positive_dy):
-    for x in (range(coords[0], coords[0] + radius + 1) if positive_dx else range(coords[0] - radius, coords[0] + 1)):
-        for y in (range(coords[1], coords[1] + radius + 1) if positive_dy else range(coords[1] - radius,
-                                                                                     coords[1] + 1)):
-            if (x - coords[0]) ** 2 + (y - coords[1]) ** 2 > radius ** 2:
-                image[x, y] = Color('#FFFFFF00')
+    for x in (
+        range(coords[0], coords[0] + radius + 1)
+        if positive_dx
+        else range(coords[0] - radius, coords[0] + 1)
+    ):
+        for y in (
+            range(coords[1], coords[1] + radius + 1)
+            if positive_dy
+            else range(coords[1] - radius, coords[1] + 1)
+        ):
+            if (x - coords[0]) ** 2 + (y - coords[1]) ** 2 > radius**2:
+                image[x, y] = Color("#FFFFFF00")
 
 
 def round_corners(image, radius):
     round_corner(image, radius, (radius, radius), False, False)
     round_corner(image, radius, (image.width - 1 - radius, radius), True, False)
     round_corner(image, radius, (radius, image.height - 1 - radius), False, True)
-    round_corner(image, radius, (image.width - 1 - radius, image.height - 1 - radius), True, True)
+    round_corner(
+        image, radius, (image.width - 1 - radius, image.height - 1 - radius), True, True
+    )
 
 
 def complete_width(image):
@@ -168,6 +198,8 @@ def complete_width(image):
     if total_width // 128 % 2 == 0:
         total_width += 128
 
-    formatted_image = Image(width=total_width, height=128, background=Color('#FFFFFF00'))
+    formatted_image = Image(
+        width=total_width, height=128, background=Color("#FFFFFF00")
+    )
     formatted_image.composite(image, left=(total_width - map_width) // 2)
     return formatted_image
